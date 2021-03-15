71acea8270e8bdb1ea8ca23cb85d03f6e582fc35
Vaynbaum <78900834+Vaynbaum@users.noreply.github.com>
2021-03-15 14:41:33 +0300

    Вайнбаум

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int i = 0, j = size - 1, countNum = 0;
-    while (i < j){
+    while (i < j) {
         int mid = (i + j) / 2;
         if (arr[mid] < value)
             i = mid+1;
@@ -10,8 +10,8 @@ int cbinsearch(int *arr, int size, int value) {
             j = mid;
     }
 
-    if (arr[i] == value){
-        while (arr[i] == value){
+    if (arr[i] == value) {
+        while (arr[i] == value) {
             countNum++;
             i++;
         }
