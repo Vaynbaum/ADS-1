cf3e283f661c2d4631270e1e668c8723fe7901e0
Vaynbaum <78900834+Vaynbaum@users.noreply.github.com>
2021-03-15 14:33:22 +0300

    Вайнбаум

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int i = 0, j = size - 1, countNum = 0;
+    while (i < j)
+    {
+        int mid = (i + j) / 2;
+        if (arr[mid] < value)
+            i = mid+1; 
+        else
+            j = mid;
+    }
+
+    if (arr[i] == value)
+    {
+        while (arr[i] == value)
+        {
+            countNum++;
+            i++;
+        }
+    }
+
+    if (countNum)
+        return countNum;
+    else
+        return 0;
 }
